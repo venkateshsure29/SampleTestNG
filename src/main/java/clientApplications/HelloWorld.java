df846e97d1c1ea0a06d358bf0b373dac4f734354
Saradhi Seshagiri <SS95957@TechMahindra.com>
2017-08-18 17:42:07 +0530

    Added the files on 18th Aug, 2017 @ 5:41 pm...

---
+++ src/main/java/clientApplications/HelloWorld.java
@@ -0,0 +1,17 @@
+package clientApplications;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext hw = SpringApplication.run(HelloWorld.class, args);
+		System.out.println("Hello Spring Boot....");
+	
+	}
+
+}
